5cca0ff31251aa1fafdeb7d9a078b91f26e4f0a8
DEV-110 <128716740+DEV-110@users.noreply.github.com>
2024-07-01 21:09:30 +0100

    added egydead and akwam

---
+++ AkwamProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+version = 1
+
+cloudstream {
+    description = ""
+    authors = listOf( "Blatzar" )
+	
+	language = "ar"
+	
+    status = 1
+
+    tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "Cartoon" )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=akwam.to&sz=%size%"
+}
