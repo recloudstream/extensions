bf168de2213669fa2f4064e33bd585fe81bf1ae8
Luna712 <142361265+Luna712@users.noreply.github.com>
2024-09-11 07:48:38 -0600

    Add DailymotionProvider (#17)

--- TwitchProvider/build.gradle.kts
+++ TwitchProvider/build.gradle.kts
@@ -17,7 +17,7 @@ cloudstream {
     status = 1 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
+    // You can find a list of available types here:
     // https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("Live")
     iconUrl = "https://www.google.com/s2/favicons?domain=twitch.tv&sz=%size%"
