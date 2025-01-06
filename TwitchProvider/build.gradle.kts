90b1ea4c6b5fd755d64412b682acd5650812d634
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-06 16:36:05 -0700

    Cleanup

--- TwitchProvider/build.gradle.kts
+++ TwitchProvider/build.gradle.kts
@@ -1,8 +1,8 @@
-// use an integer for version numbers
+// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove them
+    // All of these properties are optional, you can safely remove them.
 
     description = "Watch livestreams from Twitch"
     authors = listOf("CranberrySoup")
@@ -13,12 +13,9 @@ cloudstream {
     * 1: Ok
     * 2: Slow
     * 3: Beta only
-    * */
-    status = 1 // will be 3 if unspecified
+    **/
+    status = 1 // Will be 3 if unspecified
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of available types here:
-    // https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("Live")
     iconUrl = "https://www.google.com/s2/favicons?domain=twitch.tv&sz=%size%"
 }
