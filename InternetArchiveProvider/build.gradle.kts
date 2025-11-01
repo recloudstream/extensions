03d3f4403a6c64ac55a2d1994eaaa09f559b3417
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-01 10:07:29 -0600

    Update

--- InternetArchiveProvider/build.gradle.kts
+++ InternetArchiveProvider/build.gradle.kts
@@ -8,12 +8,12 @@ cloudstream {
     authors = listOf("Luna712")
 
     /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     */
     status = 1 // Will be 3 if unspecified
 
     tvTypes = listOf("Others")
